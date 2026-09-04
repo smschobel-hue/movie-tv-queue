dc19dc9d4d0d6b0da92f179afae316c6b0d5e23f
smschobel-hue <smschobel@verizon.net>
2026-09-04 13:58:32 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+
+  return (
+
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+
+      <h1>Movie & TV Queue</h1>
+
+      <p>The app is working!</p>
+
+    </main>
+
+  );
+
+}
